ece5d27549558b94d9fb2beb5f6ce7c38afc4163
Alif <alifa.musthafa@go-jek.com>
2021-07-31 13:31:42 +0700

    rename main activity to UserFinderActivity.kt

---
+++ app/src/main/java/com/aliftc12/githubuserfinder/presentation/UserFinderActivity.kt
@@ -0,0 +1,12 @@
+package com.aliftc12.githubuserfinder.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aliftc12.githubuserfinder.R
+
+class UserFinderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_finder)
+    }
+}
