4977dd43576e2d0c11da15af9e0980fb7250b421
Alif <alifa.musthafa@go-jek.com>
2021-07-31 19:03:29 +0700

    change return type of searchUser into ResourceState<PageMeta<GithubUsers>>

--- app/src/main/java/com/aliftc12/githubuserfinder/domain/repository/UserRepository.kt
+++ app/src/main/java/com/aliftc12/githubuserfinder/domain/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package com.aliftc12.githubuserfinder.domain.repository
 
-import com.aliftc12.githubuserfinder.domain.model.GithubUser
+import com.aliftc12.githubuserfinder.domain.model.GithubUsers
+import com.aliftc12.githubuserfinder.domain.model.PageMeta
+import com.aliftc12.githubuserfinder.domain.model.ResourceState
 
 interface UserRepository {
-   suspend fun searchUser(page: Int, query: String): List<GithubUser>
+    suspend fun searchUser(page: Int, query: String): ResourceState<PageMeta<GithubUsers>>
 }
