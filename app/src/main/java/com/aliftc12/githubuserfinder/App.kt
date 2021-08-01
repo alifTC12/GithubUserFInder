cec0418208c73596963b02202faf5f611270a9b6
Alif <alifa.musthafa@go-jek.com>
2021-08-01 22:40:23 +0700

    store app context to koin

--- app/src/main/java/com/aliftc12/githubuserfinder/App.kt
+++ app/src/main/java/com/aliftc12/githubuserfinder/App.kt
@@ -2,6 +2,7 @@ package com.aliftc12.githubuserfinder
 
 import android.app.Application
 import com.aliftc12.githubuserfinder.di.userFinderModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
@@ -9,6 +10,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(this@App)
             modules(userFinderModule)
         }
     }
