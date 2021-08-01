1507516fdde2bba522bba056349d0af062eaf8cc
Alif <alifa.musthafa@go-jek.com>
2021-08-01 22:44:04 +0700

    add HaveNoResult and Succeed state

--- app/src/main/java/com/aliftc12/githubuserfinder/presentation/SearchUserState.kt
+++ app/src/main/java/com/aliftc12/githubuserfinder/presentation/SearchUserState.kt
@@ -1,19 +1,17 @@
 package com.aliftc12.githubuserfinder.presentation
 
-import com.aliftc12.githubuserfinder.domain.model.GithubUsers
-import com.aliftc12.githubuserfinder.domain.model.PageMeta
-
 sealed class SearchUserState {
     // initial state, when list empty
     object Loading : SearchUserState()
-    data class Succeed(val pageMetaGithubUsers: PageMeta<GithubUsers>) : SearchUserState()
+    object Succeed : SearchUserState()
+    object HaveNoResult : SearchUserState()
     object Failed : SearchUserState()
 
     // load more state
     sealed class LoadMoreState() : SearchUserState() {
         object Loading : LoadMoreState()
         object Failed : LoadMoreState()
-        data class Succeed(val users: GithubUsers) : LoadMoreState()
+        object Succeed : LoadMoreState()
         object AllDataLoaded : LoadMoreState()
     }
 }
