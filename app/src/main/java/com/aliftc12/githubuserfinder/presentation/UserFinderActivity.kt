adef22dec18580f21630d5bda2eeaea8ed759b6c
Alif <alifa.musthafa@go-jek.com>
2021-08-01 22:44:42 +0700

    implement userListAdapter to activity

--- app/src/main/java/com/aliftc12/githubuserfinder/presentation/UserFinderActivity.kt
+++ app/src/main/java/com/aliftc12/githubuserfinder/presentation/UserFinderActivity.kt
@@ -1,12 +1,80 @@
 package com.aliftc12.githubuserfinder.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.aliftc12.githubuserfinder.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.aliftc12.githubuserfinder.databinding.ActivityUserFinderBinding
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+private const val VISIBLE_THRESHOLD = 2
 
 class UserFinderActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserFinderBinding
+    private val viewModel: UserFinderViewModel by viewModel()
+    private val userListAdapter: UserListAdapter by inject()
+
+    private val endlessLinearScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = binding.userList.layoutManager as LinearLayoutManager
+            val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+            val totalCurrentItem = layoutManager.itemCount
+
+            if (lastVisibleItemPosition + VISIBLE_THRESHOLD > totalCurrentItem) {
+                viewModel.loadMoreUser(binding.inputEt.text.toString(), totalCurrentItem)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_finder)
+        binding = ActivityUserFinderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpObserver()
+        setupUi()
+    }
+
+    private fun setUpObserver() {
+        viewModel.searchUserState.observe(this) { state ->
+            Log.d("searchUserState", state.toString())
+            when (state) {
+                SearchUserState.Failed -> {
+                }
+                SearchUserState.Loading -> {
+                }
+                SearchUserState.Succeed -> {
+                }
+                SearchUserState.HaveNoResult -> {
+                }
+
+                SearchUserState.LoadMoreState.AllDataLoaded -> {
+                }
+                SearchUserState.LoadMoreState.Failed -> {
+                }
+                SearchUserState.LoadMoreState.Loading -> {
+                }
+                SearchUserState.LoadMoreState.Succeed -> {
+                }
+            }
+        }
+
+        viewModel.githubUsers.observe(this) { githubUsers ->
+            userListAdapter.submitList(githubUsers.toList())
+        }
+    }
+
+    private fun setupUi() = with(binding) {
+        userList.apply {
+            addOnScrollListener(endlessLinearScrollListener)
+            adapter = userListAdapter
+        }
+        searchBtn.setOnClickListener {
+            viewModel.searchUser(binding.inputEt.text.toString())
+        }
     }
 }
