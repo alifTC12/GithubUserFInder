c4b27f613caa71d304ed3c2493f2a24fc2910101
Alif <alifa.musthafa@go-jek.com>
2021-08-01 10:11:26 +0700

    register ui states of search user to SearchUserState.kt

---
+++ app/src/main/java/com/aliftc12/githubuserfinder/presentation/SearchUserState.kt
@@ -0,0 +1,19 @@
+package com.aliftc12.githubuserfinder.presentation
+
+import com.aliftc12.githubuserfinder.domain.model.GithubUsers
+import com.aliftc12.githubuserfinder.domain.model.PageMeta
+
+sealed class SearchUserState {
+    // initial state, when list empty
+    object Loading : SearchUserState()
+    data class Succeed(val pageMetaGithubUsers: PageMeta<GithubUsers>) : SearchUserState()
+    object Failed : SearchUserState()
+
+    // load more state
+    sealed class LoadMoreState() : SearchUserState() {
+        object Loading : LoadMoreState()
+        object Failed : LoadMoreState()
+        data class Succeed(val user: GithubUsers) : LoadMoreState()
+        object AllDataLoaded : LoadMoreState()
+    }
+}
